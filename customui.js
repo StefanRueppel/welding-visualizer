ec3e5546e792c7e56d03e8f2f9ca356e7381eb68
heimmat <git.heiming@outlook.com>
2017-04-03 17:26:13 +0200

    fix parentheses error

--- customui.js
+++ customui.js
@@ -3,10 +3,10 @@ $(document).ready(documentInit);
 function documentInit() {
     getAllMaterialAttributes().then(function(attributes) {
         for (let attribute of attributes) {
-            $(".axis-select").append($("<option>"), {
+            $(".axis-select").append($("<option>", {
                 value: attribute.attributeId,
                 text: attribute.name + "[" + attribute.unit + "]"
-            });
+            }));
         }
     }, function(err) {
 
