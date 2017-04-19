c2c2532042812b4df3fc62f1f45832020e8af7f0
heimmat <git.heiming@outlook.com>
2017-04-19 17:47:17 +0200

    fix typo where xhr.onload is treated as function

--- serverapi.js
+++ serverapi.js
@@ -11,7 +11,7 @@ function getAllMaterialAttributes() {
 function getMaterialsWithAttributes(xAxisAttribute, yAxisAttribute) {
     return new Promise(function(resolve, reject) {
         let xhr = new XMLHttpRequest();
-        xhr.onload() = (evt) => resolve(JSON.parse(evt.target.responseText));
+        xhr.onload = (evt) => resolve(JSON.parse(evt.target.responseText));
         xhr.onerror = () => reject("Error while contacting server");
         xhr.open("GET", "materials.php?xattr=" + xAxisAttribute + "&yattr=" + yAxisAttribute);
         xhr.send();
