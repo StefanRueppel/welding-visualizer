e38ee104d064f874ddcf8b9b11739b60e7a04bff
heimmat <git.heiming@outlook.com>
2017-04-22 13:56:54 +0200

    restructure code to use generic jsonAJAX

--- serverapi.js
+++ serverapi.js
@@ -1,7 +1,14 @@
-function getAllMaterialAttributes() {
+/*function getAllMaterialAttributes() {
     return new Promise(function(resolve, reject) {
         let xhr = new XMLHttpRequest();
-        xhr.onload = (evt) => resolve(JSON.parse(evt.target.responseText));
+        xhr.onload = function(evt) {
+            if (this.status === 404) {
+                console.error("404 - Resource " + resourceURL + " not found");       
+            }
+            else {
+                resolve(JSON.parse(evt.target.responseText));
+            }
+        }
         xhr.onerror = () => reject("Error while contacting server");
         xhr.open("GET", "attributes.php");
         xhr.send();
@@ -9,11 +16,44 @@ function getAllMaterialAttributes() {
 }
 
 function getMaterialsWithAttributes(xAxisAttribute, yAxisAttribute) {
+    let resourceURL = "materials.php";
     return new Promise(function(resolve, reject) {
         let xhr = new XMLHttpRequest();
-        xhr.onload = (evt) => resolve(JSON.parse(evt.target.responseText));
+        xhr.onload = function(evt) {
+            if (this.status === 404) {
+                console.error("404 - Resource " + resourceURL + " not found");
+            }
+            else {
+                resolve(JSON.parse(evt.target.responseText));
+            }
+        };
         xhr.onerror = () => reject("Error while contacting server");
-        xhr.open("GET", "materials.php?xattr=" + encodeURIComponent(xAxisAttribute) + "&yattr=" + encodeURIComponent(yAxisAttribute));
+        xhr.open("GET", resourceURL + "?xattr=" + encodeURIComponent(xAxisAttribute) + "&yattr=" + encodeURIComponent(yAxisAttribute));
         xhr.send();
+    });
+}*/
+
+function getAllMaterialAttributes() {
+    return jsonAJAX("attributes.php", "GET");
+}
+
+function getMaterialsWithAttributes(xAxisAttribute, yAxisAttribute) {
+    return jsonAJAX("materials.php", "GET", "xattr=" + encodeURIComponent(xAxisAttribute) + "&yattr=" + encodeURIComponent(yAxisAttribute));
+}
+
+function jsonAJAX(resourceURL, method, data=null) {
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.onload = function(evt) {
+            if (this.status === 404) {
+                reject("404 - Resource " + resourceURL + " not found");
+            }
+            else {
+                resolve(JSON.parse(evt.target.responseText));
+            }
+        };
+        xhr.onerror = () => reject("Network error");
+        xhr.open(method, resourceURL + (method === "GET" && data ? "?" + data : ""));
+        xhr.send(method === "POST" ? data : null);
     });
 }
